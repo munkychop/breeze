50a4cab041109e2286bd1f1481b8aa900b586c62
Ivan <git@ivanhayes.com>
2014-10-24 18:17:30 +0100

    added Grunt build step.

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt) {
+
+	"use strict";
+
+	grunt.loadNpmTasks("grunt-contrib-sass");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-connect");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+
+	grunt.initConfig({
+
+		sass: {
+
+			dev: {
+				options: {
+					style: "compressed",
+					sourcemap : true
+				},
+
+				files : {
+					"dist/css/app.min.css": "src/scss/app.scss"
+				}
+			}
+		},
+
+		uglify: {
+
+			dev: {
+				options: {
+					compress: true,
+					mangle: true,
+					preserveComments: false
+				},
+
+				files: {
+					"dist/js/app.min.js" : ["src/js/libs/atomic.js", "src/js/app/app.js"]
+				}
+			}
+		},
+
+		connect: {
+
+			dev : {
+				options: {
+					open: true,
+					keepalive: true
+				}
+			}
+		},
+
+		watch: {
+			
+			js: {
+				files: ["src/js/**/*.js"],
+				tasks: ["uglify:dev"]
+			},
+
+			scss: {
+				files: ["src/scss/**/*.scss"],
+				tasks: ["sass:dev"]
+			}
+		}
+	});
+};
