3e289d3d1435d49a98ace2b47563aada2ce66711
Ivan Hayes <git@ivanhayes.com>
2014-11-17 11:05:32 +0000

    removed 'keepalive' prop from grunt-contrib-connect task.

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,9 +1,36 @@
-// 0°C === 32°F.
-var ZERO_DEG_CELCIUS_IN_FAHRENHEIT = 32;
-var CONVERSION_MULTIPLIER = 1.8;
+// -------------------------------------------------------
+// Create a closure to ensure that we don't pollute the
+// global scope.
+// -------------------------------------------------------
+(function (){
+	
+	// -------------------------------------------------------
+	// Define constants.
+	// -------------------------------------------------------
 
-function fahrenheitToCelcius (fahrenheit)
-{
-	var celcius = (fahrenheit - ZERO_DEG_CELCIUS_IN_FAHRENHEIT) / CONVERSION_MULTIPLIER;
-	return celcius.toFixed(1);
-}
+	// 0°C === 32°F.
+	var ZERO_DEG_CELCIUS_IN_FAHRENHEIT = 32;
+	var CONVERSION_MULTIPLIER = 1.8;
+
+	// Add an event listener to ensure the DOM has loaded before
+	// initialising the application.
+	document.addEventListener("DOMContentLoaded", init);
+
+	// -------------------------------------------------------
+	// Initialisation function.
+	// -------------------------------------------------------
+	function init ()
+	{
+		console.log("init");
+	}
+
+	// -------------------------------------------------------
+	// Utility functions.
+	// -------------------------------------------------------
+	function fahrenheitToCelcius (fahrenheit)
+	{
+		var celcius = (fahrenheit - ZERO_DEG_CELCIUS_IN_FAHRENHEIT) / CONVERSION_MULTIPLIER;
+		return celcius.toFixed(1);
+	}
+
+})();
